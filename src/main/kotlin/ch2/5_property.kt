e391e6874c69af9e13cb35aadd6f93c9079885f0
byeungoo <dksekfldks65@naver.com>
2024-01-16 23:20:08 +0900

    주석 추가

--- src/main/kotlin/ch2/5_property.kt
+++ src/main/kotlin/ch2/5_property.kt
@@ -3,7 +3,7 @@ package ch2
 class Rectangle(val height : Int, val width : Int) {
 
     /**
-     * 클래스 특성을 정의하고 싶다면 프로퍼티로 정의
+     * 메소드로 작성해도 성능상 차이는 없지만 클래스 특성을 정의하고 싶다면 프로퍼티로 정의
      */
     val isSquare: Boolean
         get () {    // 커스텀 접근자
