41e7327cd48f3fa5b75dbdb54906458ebe308679
byeungoo <dksekfldks65@naver.com>
2024-01-17 22:44:00 +0900

    maxOrNull로 수정

--- src/main/kotlin/ch3/1_create_collection.kt
+++ src/main/kotlin/ch3/1_create_collection.kt
@@ -2,7 +2,7 @@ package ch3
 
 fun main() {
     val set = hashSetOf(1, 7, 13)
-    //println(set.max())
+    println(set.maxOrNull())
     val list = arrayListOf(1,7,53)
     val map = hashMapOf(1 to "one", 53 to "fifty-three")
     val numList = listOf(1,2,3,4)
