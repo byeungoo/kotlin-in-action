fa3f6de1b3c007dbcaa593fadfea47e0ad9cdb4e
byeungoo <dksekfldks65@naver.com>
2024-01-17 23:37:22 +0900

    정규식 예제 추가

--- src/main/kotlin/ch3/8_문자열다루기.kt
+++ src/main/kotlin/ch3/8_문자열다루기.kt
@@ -8,8 +8,18 @@ fun parsePath(path: String) {
     println("Dir: $directory, name: $fileName, ext: $extension")
 }
 
+fun parsePathWithReges(path:String) {
+    val regex = """(.+)/(.+)\.(.+)""".toRegex()
+    val matchResult = regex.matchEntire(path)
+    if(matchResult != null) {
+        val (directory, filename, extension) = matchResult.destructured
+        println("Dir: $directory, name: $filename, ext: $extension")
+    }
+}
+
 fun main() {
     println("asd.asdas.qwewq".split("."))   // "." 단위로 나눠서 배열 반환
     println("asd.as-das.qwewq".split(".", "-"))   // ".", "-" 단위로 나눠서 배열 반환
-    parsePath("/Users/yole/kotlin-book/chapter.adoc");
+    parsePath("/Users/yole/kotlin-book/chapter.adoc")
+    parsePathWithReges("/Users/yole/kotlin-book/chapter.adoc")
 }
